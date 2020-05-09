decabb0903c94c4708a97613406ce3131a283beb
Bao <bclu2104@gmail.com>
2020-05-10 01:42:28 +0800

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+
+(function(window){
+var speakWord = "Good Bye";
+var byeSpeaker = {}
+byeSpeaker.speak = function () {
+  console.log(speakWord + " " + byeSpeaker.name);
+}
+window.byeSpeaker = byeSpeaker
+})(window)
